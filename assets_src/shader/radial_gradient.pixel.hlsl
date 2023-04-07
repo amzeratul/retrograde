72e967629f781b7168d4a321ed2353708d832d7b
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2023-04-07 22:15:01 +0100

    Improved gradient bg shader

--- assets_src/shader/radial_gradient.pixel.hlsl
+++ assets_src/shader/radial_gradient.pixel.hlsl
@@ -6,8 +6,10 @@ float randomNoise(float2 seed)
 }
 
 float4 main(VOut input) : SV_TARGET {
-    float2 coord = input.texCoord0.xy * 2.0 - float2(1, 1);
-    float dist = length(coord) / sqrt(2);
+    float ar = ddx(input.texCoord0.x) / ddy(input.texCoord0.y);
+
+    float2 coord = (input.texCoord0.xy * 2.0 - float2(1, 1)) / float2(ar, 1);
+    float dist = length(coord) / sqrt(1.0 / ar + 1.0);
     float4 edgeCol = float4(input.colour.rgb, 1.0);
     float4 midCol = float4(0.14, 0.11, 0.11, 1.0); // Hack
     float n = randomNoise(input.position) / 255.0;
